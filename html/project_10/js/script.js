2a17d2b4146b987cb00a295183e74157d6d1b4c0
Farihan_Rashid <farihanrashid3@gmail.com>
2021-02-01 09:41:25 +0600

    project-10 v2.5

--- html/project_10/js/script.js
+++ html/project_10/js/script.js
@@ -1,23 +1,24 @@
 $(function(){
-    // back to top
+    // back to top js
     $('.back-to-top').on('click', function(){
         $('html, body').animate({scrollTop:0},1000);
     });
     
     $(window).on('scroll', function(){
         var scrolling = $(this).scrollTop();
-    
-        if(scrolling > 300){
+        var viewportHeight= window.innerHeight-100 ;
+        var viewportHeight200= viewportHeight + 150;
+        if(scrolling > viewportHeight200){
             $('.back-to-top').fadeIn(500);
         }
         else {
             $('.back-to-top').fadeOut(500);
         }
-        if(scrolling > 500){
-            $('.navbar').addClass('bg');
+        if(scrolling > viewportHeight){
+            $('#header_sec').addClass('bg');
         }
         else {
-            $('.navbar').removeClass('bg');
+            $('#header_sec').removeClass('bg');
         }
     
     });
@@ -30,14 +31,14 @@ $(function(){
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     html_body.animate({
-                        scrollTop: target.offset().top - 0
+                        scrollTop: target.offset().top - 90
                     }, 1500);
                     return false;
                 }
             }
         });
     
-    
+        // preloader js
        $(window).on('load', function(){
             $('.preloader').delay(2000).fadeOut(500);
             $('html, body').delay(2000).animate({scrollTop:0},1000);
