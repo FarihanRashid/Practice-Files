8b16a015053adbb39ce74482674b3f89d0411395
Farihan_Rashid <farihanrashid3@gmail.com>
2021-03-08 00:11:37 +0600

    project-13 v1.62-services-sec

--- html/project_13/js/script.js
+++ html/project_13/js/script.js
@@ -52,7 +52,7 @@ $(function () {
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 html_body.animate({
-                    scrollTop: target.offset().top - 90
+                    scrollTop: target.offset().top - 84
                 }, 1500);
                 return false;
             }
